88fd2f645c7ed5d61dc94529342d1b77ec5d0fd8
ppmasa8 <kiyokiyopip56@gmail.com>
2023-05-22 19:13:44 +0900

    Added

---
+++ simpleget/wwwformurlencoded.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	values := url.Values{
+		"test": {"value"},
+	}
+
+	resp, err := http.PostForm("http://localhost:18888", values)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+	log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
+}
